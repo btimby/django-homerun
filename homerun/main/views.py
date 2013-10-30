3763cc853b01552c25020d62cc2e58d0e9208dd9
Ben Timby <btimby@gmail.com>
2013-10-29 23:13:49 -0400

    CSRF exempt for IVR views. A bit more user friendly call flow.

--- homerun/main/views.py
+++ homerun/main/views.py
@@ -4,6 +4,8 @@ from twilio import twiml
 
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from django.views.generic.base import View
 
 from django.contrib.auth.models import User
@@ -12,6 +14,7 @@ from django.contrib.auth.models import User
 class IVRView(View):
     logger = logging.getLogger('django.request')
 
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, username, **kwargs):
         response = twiml.Response()
         try:
@@ -28,13 +31,15 @@ class IVRView(View):
 
 
 class IVRMenuView(IVRView):
-    options = None
+    menu = None
 
     def get(self, request, response):
-        if self.options is None:
-            raise NotImplemented('You must define the options attribute')
+        if not self.menu:
+            raise NotImplemented('You must define the menu attribute')
+        response.say('Please make a selection from the following menu.')
         with response.gather(numDigits=1, method='POST') as r:
-            menu = ['Press %s to %s.' % (k, v) for k, v in self.options.items()]
+            menu = ['Press %s to %s.' % (k, v) for k, v in self.menu.items()]
+            r.pause(length=3)
             r.say(' '.join(menu), loop=3)
         # If the user does not make a selection, say goodbye, then hang-up
         response.say("Goodbye")
@@ -42,7 +47,7 @@ class IVRMenuView(IVRView):
 
     def post(self, request, response):
         digits = request.POST.get('digits')
-        if digits not in self.options:
+        if digits not in self.menu:
             response.say('You have chosen an invalid option.')
             response.redirect(request.build_absolute_uri())
             return
@@ -58,6 +63,10 @@ class IVRMainMenuView(IVRMenuView):
         '1': 'hear a list of available homes',
         '2': 'reach a customer service representative',
     }
+
+    def get(self, request, response):
+        response.say('Thank you for calling.')
+        return super(IVRMainMenuView, self).get(request, response)
 
     def handle_1(self, request, response):
         response.say("Listing now.")
