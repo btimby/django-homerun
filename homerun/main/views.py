21ebd6be8ea3951744eaf59e15ada914697f337f
Ben Timby <btimby@gmail.com>
2013-10-29 23:46:36 -0400

    Extend the twiml.Response and twiml.Gather objects a bit to allow a voice to be set for the entire call. Probably would be nice upstream.

--- homerun/main/views.py
+++ homerun/main/views.py
@@ -11,24 +11,56 @@ from django.views.generic.base import View
 from django.contrib.auth.models import User
 
 
-VOICE = 'woman'
+class TwiMLGather(twiml.Gather):
+    def __init__(self, *args, **kwargs):
+        self.voice = kwargs.pop('voice', None)
+        super(TwiMLGather, self).__init__(*args, **kwargs)
+
+    def say(self, *args, **kwargs):
+        # TODO: make this a mixin, shared with TwiMLResponse
+        if self.voice:
+            kwargs.setdefault('voice', self.voice)
+        return super(TwiMLGather, self).say(*args, **kwargs)
+
+
+class TwiMLResponse(twiml.Response):
+    nestables = twiml.Response.nestables + ['TwiMLGather', ]
+
+    def __init__(self, *args, **kwargs):
+        self.voice = kwargs.pop('voice', None)
+        super(TwiMLResponse, self).__init__(*args, **kwargs)
+
+    def say(self, *args, **kwargs):
+        # TODO: make this a mixin, shared with TwiMLGather
+        if self.voice:
+            kwargs.setdefault('voice', self.voice)
+        return super(TwiMLResponse, self).say(*args, **kwargs)
+
+    def gather(self, *args, **kwargs):
+        if self.voice:
+            kwargs.setdefault('voice', self.voice)
+        return self.append(TwiMLGather(*args, **kwargs))
+
+    def clear(self):
+        self.verbs = []
 
 
 class IVRView(View):
     logger = logging.getLogger('django.request')
+    voice = 'woman'
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, username, **kwargs):
-        response = twiml.Response()
+        response = TwiMLResponse(voice=self.voice)
         try:
             request.user = User.objects.get(username=username)
             super(IVRView, self).dispatch(request, response, **kwargs)
         except Exception:
             self.logger.error('ERROR', exc_info=True)
             # Clear out any TwiML built so far.
-            response.verbs = []
+            response.clear()
             # Return error message.
-            response.say("Unable to handle your call, please try again later.", voice=VOICE)
+            response.say("Unable to handle your call, please try again later.")
             response.hangup()
         return HttpResponse(response.toxml(), content_type='text/xml')
 
@@ -39,12 +71,12 @@ class IVRMenuView(IVRView):
     def get(self, request, response):
         if not self.menu:
             raise NotImplemented('You must define the menu attribute')
-        response.say('Please make a selection from the following menu.', voice=VOICE)
+        response.say('Please make a selection from the following menu.')
         with response.gather(numDigits=1, method='POST') as r:
             menu = ['Press %s to %s.' % (k, v) for k, v in self.menu.items()]
             # Do our own loop, since a pause is not easy to inject between repeats.
             for i in range(3):
-                r.say(' '.join(menu), voice=VOICE)
+                r.say(' '.join(menu))
                 r.pause(length=3)
         # If the user does not make a selection, say goodbye, then hang-up
         response.say("Goodbye")
@@ -53,7 +85,7 @@ class IVRMenuView(IVRView):
     def post(self, request, response):
         digits = request.POST.get('Digits')
         if digits not in self.menu:
-            response.say('You have chosen an invalid option.', voice=VOICE)
+            response.say('You have chosen an invalid option.')
             response.redirect(request.build_absolute_uri(), method='GET')
             return
         try:
@@ -70,11 +102,11 @@ class IVRMainMenuView(IVRMenuView):
     }
 
     def get(self, request, response):
-        response.say('Thank you for calling.', voice=VOICE)
+        response.say('Thank you for calling.')
         return super(IVRMainMenuView, self).get(request, response)
 
     def handle_1(self, request, response):
-        response.say("Listing now.", voice=VOICE)
+        response.say("Listing now.")
 
     def handle_2(self, request, response):
-        response.say('Forwarding to agent.', voice=VOICE)
+        response.say('Forwarding to agent.')
